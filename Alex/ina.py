102f7835d3afa8b238776eb46c337825fbe98203
alexszabo <alex_szabo@rogers.com>
2022-03-25 19:43:55 -0400

    INA algorithm to generate csv

--- Alex/ina.py
+++ Alex/ina.py
@@ -1,9 +1,17 @@
 from time import sleep
+from datetime import datetime
 from ina219 import INA219
+import pandas as pd
 
 ina = INA219(shunt_ohms = 0.1, max_expected_amps = 0.6, address = 0x40, busnum = 1)
 
 ina.configure(voltage_range = ina.RANGE_16V, gain = ina.GAIN_AUTO, bus_adc = ina.ADC_128SAMP, shunt_adc = ina.ADC_128SAMP)
+
+dict = {'Time': [],
+        'Voltage':[],
+        'Current':[],
+        'Power':[],
+        }
 
 try:
     while 1:
@@ -11,7 +19,13 @@ try:
         i = ina.current()
         p = ina.power()
         print("\n POWER: %f \n CURRENT: %f \n VOLTAGE: %f" %(p, i, v))
-        sleep(1)
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        dict['Time'].append(current_time)
+        dict['Voltage'].append(v)
+        dict['Current'].append(i)
+        dict['Power'].append(p)
+        sleep(30)
 
 except KeyboardInterrupt:
     print("\n exiting")
