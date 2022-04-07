37d8e47fe23641e00001a4d2848f16d967dcc052
ghassanarnouk <ghassanarnouk@cmail.carleton.ca>
2022-04-07 12:28:12 -0400

    voltage tex code

--- Ghassan/scripts/solarData.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp -f solar*.csv solar.csv
-
-
---
+++ Ghassan/scripts/voltage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp -f /home/pi/Documents/ELEC-3907/Alex/solar*.csv /home/pi/Documents/ELEC-3907/Ghassan/scripts/solar.csv
+
+arara -l /home/pi/Documents/ELEC-3907/Ghassan/voltage/voltage.tex
